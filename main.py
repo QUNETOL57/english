d6e86978d1821be175a9e475699e33522a086bce
QUNETOL57 <myEmail@example.com>
2019-03-11 19:16:58 +0300

    Some corrections

--- main.py
+++ main.py
@@ -134,10 +134,12 @@ class Word(DataBase,ChoiceTSMixin,PrintSmthMixin):
 
 class Text(DataBase,ChoiceTSMixin,PrintSmthMixin):
     def text_en_ru(self):
-        self.print_smth(self.choice_theme('texts'),self.english_smth_index,self.russian_smth_index)
+        self.print_smth(self.choice_selection(self.choice_theme('texts')),
+        self.english_smth_index,self.russian_smth_index)
 
     def text_ru_en(self):
-        self.print_smth(self.choice_theme('texts'),self.russian_smth_index,self.english_smth_index)
+        self.print_smth(self.choice_selection(self.choice_theme('texts')),
+        self.russian_smth_index,self.english_smth_index)
 
 word = Word()
 word.words_en_ru()
